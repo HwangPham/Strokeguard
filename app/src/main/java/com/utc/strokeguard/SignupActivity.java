9f6a210457971a6eb273f40521308f49e5a18948
HwangPham <hoangph0310@gmail.com>
2024-09-28 10:23:37 +0700

    Initial commit

---
+++ app/src/main/java/com/utc/strokeguard/SignupActivity.java
@@ -0,0 +1,71 @@
+package com.utc.strokeguard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.utc.strokeguard.model.User;
+
+public class SignupActivity extends AppCompatActivity {
+
+    EditText signupName, signupEmail, signupPhonenumber, signupUsername, signupPassword;
+    TextView loginRedirectText;
+    Button signupButton;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+
+        signupName = findViewById(R.id.signup_name);
+        signupUsername = findViewById(R.id.signup_username);
+        signupEmail = findViewById(R.id.signup_email);
+        signupPhonenumber = findViewById(R.id.signup_phonenumber);
+        signupButton = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference();
+
+                String name = signupName.getText().toString();
+                String username = signupUsername.getText().toString();
+                String email = signupEmail.getText().toString();
+                String phonenumber = signupPhonenumber.getText().toString();
+                String password = signupPassword.getText().toString();
+
+                User user = new User(name, username, email, phonenumber, password);
+                reference.child(name).setValue(user);
+
+                Toast.makeText(SignupActivity.this, "You have signup successfully", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/utc/strokeguard/model/User.java
@@ -0,0 +1,58 @@
+package com.utc.strokeguard.model;
+
+public class User {
+
+    String name, email, phonenumber, username, password;
+
+    public User(String name, String username, String email, String phonenumber, String password) {
+        this.name = name;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
